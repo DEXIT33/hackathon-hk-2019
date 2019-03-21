5543c4c96a5501c66f1650bdcbf234668ed8b2b0
David Domkář <domkard@gmail.com>
2019-03-21 16:31:52 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,52 @@
 <template>
 <div class="container-main" v-bar="{
-  preventParentScroll: true
+  preventParentScroll: false
 }" ref="scrollbar">
   <div @scroll="handleScroll" ref="contentContainer">
     <v-container class="container-header">
       <h1 class="display-3 font-weight-black ">Všechny akce</h1>
-      <p class="title font-weight-regular">Řazeno od dnešního data</p>
+    </v-container>
+    <v-container class="container-search">
+      <v-layout align-center justify-start row fill-height>
+        <span class="text">Řazeno od:</span>
+        <v-menu
+          ref="datePickerFrom"
+          v-model="datePickerFrom"
+          :close-on-content-click="false"
+          :nudge-right="40"
+          :return-value.sync="date"
+          lazy
+          transition="scale-transition"
+          offset-y
+          full-width
+          min-width="290px"
+        >
+          <template v-slot:activator="{ on }">
+            <v-text-field
+              v-model="date"
+              prepend-icon="event"
+              readonly
+              v-on="on"
+            ></v-text-field>
+          </template>
+          <v-date-picker v-model="date" no-title scrollable>
+            <v-spacer></v-spacer>
+            <v-btn flat color="primary" @click="datePickerFrom = false">Cancel</v-btn>
+            <v-btn flat color="primary" @click="$refs.datePickerFrom.save(date)">OK</v-btn>
+          </v-date-picker>
+        </v-menu>
+        <span class="text"></span>
+      </v-layout>
+      <v-layout class="container-categories">
+        <v-select
+          v-model="value"
+          :items="eventTypeItems"
+          attach
+          chips
+          label="Kategorie"
+          multiple
+        ></v-select>
+      </v-layout>
     </v-container>
     <v-container class="container-content" ref="content">
       <masonry
@@ -28,9 +69,10 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue, Watch } from 'vue-property-decorator';
 import EventCard from '../components/EventCard.vue';
 import Firebase from 'firebase';
+import { eventTypes } from '../shared/event_types';
 
 @Component({
   components: {
@@ -39,64 +81,100 @@ import Firebase from 'firebase';
 })
 export default class Home extends Vue {
 
+  private datePickerFrom = false;
+  private date = new Date().toISOString().substr(0, 10);
+
   private columns = 4;
   private events: any[] = [];
   private places = new Map<string, any>();
 
   private loading = false;
-  private lastLimit = 0;
   private step = 20;
   private lastDate: Date = new Date();
 
+  private items: string[] = [];
+  private value: string[] = [];
+
   private created() {
-    
+
+    for (const type of eventTypes) {
+      this.items.push(type.name);
+    }
+
     window.addEventListener('resize', this.handleResize);
 
-    this.lastLimit += this.step;
-    Firebase.firestore()
-      .collection('events')
-      .orderBy('date_time_start_first')
-      .startAt(Firebase.firestore.Timestamp.fromDate(new Date()))
-      .limit(this.lastLimit)
-      .onSnapshot((snapshot) => {
-        this.loadPlaces(snapshot).then(() => {
-          for (let i = 0; i < this.step; i++) {
-            this.$set(this.events, i, snapshot.docs[i].data());
-            this.events[i].id = snapshot.docs[i].id;
-            this.lastDate = snapshot.docs[i].data().date_time_start_first;
-          }
-        });
-      });
+    this.loadNext();
+  }
+
+  @Watch('value')
+  private onValueChanged(value: string, oldValue: string) {
+    console.log('value changed');
+    this.events = [];
+    this.loadNext();
+  }
+
+  @Watch('date')
+  private onDateChanged(value: string, oldValue: string) {
+    console.log('date changed');
+    this.events = [];
+    this.lastDate = new Date(value);
+    this.loadNext();
   }
 
   private loadNext() {
     this.loading = true;
 
-    this.lastLimit += this.step;
+    const date = new Date(this.lastDate);
+    date.setMonth(date.getMonth() + 1);
+
     Firebase.firestore()
       .collection('events')
       .orderBy('date_time_start_first')
-      .startAt(this.lastDate)
-      .limit(this.lastLimit)
-      .onSnapshot((snapshot) => {
+      .startAt(Firebase.firestore.Timestamp.fromDate(date))
+      .limit(this.step).get().then((snapshot) => {
         this.loadPlaces(snapshot).then(() => {
-          for (let i = this.lastLimit - this.step; i < this.step; i++) {
-            this.$set(this.events, i, snapshot.docs[i].data());
-            this.events[i].id = snapshot.docs[i].id;
-            this.lastDate = snapshot.docs[i].data().index;
-          }
+          snapshot.docs.forEach((doc) => {
+            if (!this.checkForId(doc.id)) {
+              const data = doc.data();
+              data.id = doc.id;
+              this.events.push(data);
+            }
+          });
+          this.lastDate = this.events[this.events.length - 1].date_time_start_first;
           this.loading = false;
         });
+
       });
   }
 
-  private mounted() {
-    this.handleResize();
+  private getValueID(value: string) {
+    for (const type of eventTypes) {
+      if (value === type.name) {
+        return type.id;
+      }
+    }
+
+    return '';
   }
 
   private formatDate(date: Date) {
     // tslint:disable-next-line:max-line-length
-    return date.getDate() + '.' + date.getMonth() + '.' + date.getFullYear() + ' ' + date.getHours() + ':' + (date.getMinutes() === 0 ? '00' : date.getMinutes());
+    return date.getDate() + '.' + date.getMonth() + '.' + date.getFullYear();
+  }
+
+  private checkForId(id: string) {
+    for (const event of this.events) {
+      if (event.id === id) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+
+  private mounted() {
+    this.handleResize();
   }
 
   private handleResize() {
@@ -115,6 +193,7 @@ export default class Home extends Vue {
     this.handleResize();
 
     // @ts-ignore
+    // tslint:disable-next-line:max-line-length
     if ((this.$refs.contentContainer as Element).clientHeight - 100 < this.$vuebar.getState(this.$refs.scrollbar).barTop + this.$vuebar.getState(this.$refs.scrollbar).barHeight) {
       if (!this.loading) {
         this.loadNext();
@@ -146,6 +225,10 @@ export default class Home extends Vue {
   get cols() {
     return this.columns;
   }
+
+  get eventTypeItems() {
+    return this.items;
+  }
 }
 </script>
 
@@ -161,6 +244,11 @@ export default class Home extends Vue {
 
   width: 100%;
   max-width: initial;
+}
+
+.text {
+  margin-inline-start: 20px;
+  margin-inline-end: 20px;
 }
 
 .container-header {
@@ -183,6 +271,27 @@ export default class Home extends Vue {
   @media screen and (max-width: 959px) {
     padding-left: 25px;
     padding-right: 25px;
+  }
+}
+
+.container-search {
+  padding-left: 70px;
+  padding-right: 70px;
+  max-width: initial;
+
+  @media screen and (max-width: 959px) {
+    padding-left: 25px;
+    padding-right: 25px;
+  }
+}
+
+.container-categories {
+  padding-left: 142px;
+  padding-right: 38px;
+
+  @media screen and (max-width: 959px) {
+    padding-left: 25px;
+    padding-right: 38px;
   }
 }
 
